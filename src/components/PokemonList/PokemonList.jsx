f0a8b853c12b56312ffd1b38007e97c5b78c4d9b
amir000621 <amiransari621621@gmail.com>
2025-04-24 17:06:03 +0530

    minor adjustment

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -39,7 +39,7 @@ function PokemonList(){
             return{
                 id:pokemon.id,
                 name: pokemon.name,
-                image:(pokemon.sprites.other)? pokemon.sprites.other.dream_world.front_default:pokemonData.sprites.front_shiny,
+                image:(pokemon.sprites.other)? pokemon.sprites.other.dream_world.front_default:pokemonData.sprites.other.home,
                 types:pokemon.types
             }
         });
@@ -53,9 +53,11 @@ function PokemonList(){
        
         
     }
-    //callback and dependency array ye do chije useeffect expect krta hia 
-    //call back ke andar ham jo bhi logic likhenge vo execute hoga pehli baar to jab execute hoga jab component pehli baar return hoga
-    //ye khali array call back ko detect krne ke liye 
+    //callback and dependency array ye do chije useeffect expect krta hia like this-> useEffect(()=>{},[])
+    //call back ke andar ham jo bhi logic likhenge vo execute hoga pehli baar to jab execute hoga jab component pehli baar render hoga,jab bhi component re-render hoga ye dobara execute hoga
+
+    //ye khali array(its is called dependency array) call back ko detect krne ke liye 
+
     //ye khali array ka mean useffect kisi pr bhi dependent nhi hai agar mien ye array hata deta hu to ye useeffect baar baar call hota hai inspect mien dekho or agar miein empty rakhta hu to sirf ek baar call hota hai jab vo pehli baar render hua tha agar mein is empty array ke andar x ya y rakhdu to to sirf x ya f ke increment pr ye useeffect call hoga  
 
 
@@ -65,7 +67,7 @@ function PokemonList(){
         downloadPokemons();
 
     },[pokemonListState.pokedexUrl]);
-    //use effect name ke hook hame help krta hai ki kisi component ke render pr tumhe kya krna hai uske dobara render pr  kya krna chahte hai
+    //use effect name ke hook hame help krta hai ki kisi component ke render pr tumhe kya krna hai uske dobara render pr  kya krna chahte hai ,,agar ye array na ho to first ke sath sare render pr exexute hoga empty hone pr sirf pehle render pr execute hoga and agar kuchh pass kro to first render ke sath sath us state ya event jo bhi is array ke andat de rahe hai uske render pr ye bhi execute hoga
 
    
     return(
@@ -82,7 +84,7 @@ function PokemonList(){
     </div>
     <div className="controls" >
     {/* undefined ki jagah null bhi daal sakte hai */}
-        <button disabled = { pokemonListState.prevUrl === undefined} onClick={()=>setPokemonListState({...pokemonListState,pokedexUrl:pokemonListState.prevUrl})} className="controls" >Prev</button>
+        <button disabled = { pokemonListState.prevUrl === null} onClick={()=>setPokemonListState({...pokemonListState,pokedexUrl:pokemonListState.prevUrl})} className="controls" >Prev</button>
         <button disabled = { pokemonListState.nextUrl === undefined} onClick={()=>setPokemonListState({...pokemonListState,pokedexUrl:pokemonListState.nextUrl})}className="controls">Next</button>
     </div>
        </div>
